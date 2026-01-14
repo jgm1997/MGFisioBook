d76ad5e9f901cb75b612346938292c19e3d4b935
Javier González Martín <jgm_1997@protonmail.com>
2026-01-14 20:16:59 +0100

    fix: entrypoint

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# This script runs database migrations and starts the app
+
+# Run database migrations then start the app
 alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
