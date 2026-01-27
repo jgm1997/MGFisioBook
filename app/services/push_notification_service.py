8cfca1c4b6ba5989442b2a8bc89d7763d77a25f3
Javier González Martín <javier.gonzalez@omniaccess.com>
2026-01-27 16:47:52 +0100

    fix: add appointment ID in push notification

--- app/services/push_notification_service.py
+++ app/services/push_notification_service.py
@@ -2,6 +2,7 @@ from firebase_admin import messaging
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.models import appointment
 from app.models.device import Device
 
 
@@ -11,9 +12,18 @@ async def send_push_to_user(db: AsyncSession, user_id: str, title: str, body: st
     tokens = [row.token for row in result.scalars().all()]
     if not tokens:
         return
+    appointment_query = select(appointment.Appointment).where(
+        appointment.Appointment.patient_id == user_id
+    )
+    appointment_result = await db.execute(appointment_query)
+    appointment_obj = appointment_result.scalar_one_or_none()
+    if not appointment_obj:
+        return
 
     message = messaging.MulticastMessage(
-        notification=messaging.Notification(title=title, body=body), tokens=tokens
+        notification=messaging.Notification(title=title, body=body),
+        tokens=tokens,
+        data={"appointmentId": appointment_obj.ids},
     )
 
     messaging.send_each_for_multicast(message)
